3b90879d8bd2ff06183933c2829aabf9829cd8a6
Evgeniy-Shikhalev <89658389+Evgeniy-Shikhalev@users.noreply.github.com>
2022-04-07 08:38:04 +0500

    Add files via upload

---
+++ portfolio/Stone/js/script.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+    
+    // ******scroll
+  
+    $(window).scroll(function() {
+  
+      if ($(this).scrollTop() > 800) {
+          $('.scroll_up').fadeIn();
+      } else {
+          $('.scroll_up').fadeOut();
+      }
+      
+      nav.removeClass('active');
+       
+    });
+  
+  
+    // ******burger-menu
+  
+    let nav = $('#nav');
+    let navToggle = $('#navToggle');
+  
+    navToggle.on('click', function(){
+      nav.toggleClass('active');
+     
+  
+    })
+  
+  });
+  
